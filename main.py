23a9eb86f1e66398741d08af08dd455397a4912b
ConorWant <conorwant95@gmail.com>
2025-10-10 11:00:37 +0100

    [Practice] further practice with tkinter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
-counter = [0] 
+counter = [0]
+
 
 def change_text():
     label.config(text="Button Clicked!")
@@ -9,20 +10,30 @@ def increment_counter():
     counter[0] += 1  
     label2.config(text=f"Counter: {counter[0]}")
 
+def submit():
+    print(entry.get())
+    label3.config(text=f"You entered: {entry.get()}")
+    entry.delete(0, tk.END)
+    
 root = tk.Tk()
 root.title("Hello Tkinter")
-root.geometry("300x300")
+root.geometry("500x500")
 
 label = tk.Label(root, text="Welcome to Tkinter!")
-label.pack(pady=20)
-
 label2 = tk.Label(root, text=f"Counter: {counter[0]}")
-label2.pack(pady=10)
-
 button = tk.Button(root, text="Click Me", command=change_text)
-button.pack(pady=10)
-
 button2 = tk.Button(root, text="Add 1", command=increment_counter)
-button2.pack(pady=10)
+entry = tk.Entry(root)
+submit_button = tk.Button(root, text="Submit", command=submit)
+label3 = tk.Label(root)
+
+label.grid(row=0, column=3)
+label2.grid(row=1, column=2)
+button.grid(row=2, column=1)
+button2.grid(row=3, column=0)
+entry.grid(row=4, column=0, sticky="nswe")
+submit_button.grid(row=4, column=1)
+label3.grid(row=4, column=2)
+
 
 root.mainloop()
