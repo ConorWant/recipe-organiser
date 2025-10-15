b0b0691a9613897dc0c5fc81da17136373f9c933
conor <c.barron4853@student.leedsbeckett.ac.uk>
2025-10-15 09:55:22 +0100

    [PRACTICE] Added load and save popup boxes. Need to fix loading and saving logic

--- note_saver.py
+++ note_saver.py
@@ -1,15 +1,12 @@
 import tkinter as tk
 
-from pandas.core.apply import frame_apply
-
-
-def save_note():
-    with open("note.txt", 'w') as f:
-        f.write(text_box.get(1.0, tk.END))
+def save_note(filename, content):
+    with open(filename, 'w') as f:
+        f.write(content)
         clear_text_box()
 
-def load_note():
-    with open("note.txt", 'r') as f:
+def load_note(filename):
+    with open(filename, 'r') as f:
         clear_text_box()
         content = f.read()
         text_box.insert(tk.END, content)
@@ -17,6 +14,60 @@ def load_note():
 
 def clear_text_box():
     text_box.delete("1.0", tk.END)
+
+def load_popup():
+    popup = tk.Toplevel(root)
+    popup.title("Load")
+    popup.geometry("300x200")
+
+    label = tk.Label(popup, text="Enter File Name To Load:")
+    label.pack()
+
+    entry = tk.Entry(popup, width=40)
+    entry.pack()
+
+    error_label = tk.Label(popup, text="", fg="red")
+    error_label.pack()
+
+    def on_load():
+        filename = entry.get()
+        if filename:
+            try:
+                load_note(filename)
+                popup.destroy()
+            except FileNotFoundError:
+                error_label.config(text="File not found.")
+        else:
+            error_label.config(text="Please enter a filename.")
+
+    tk.Button(popup, text="Load", command=on_load).pack()
+    tk.Button(popup, text="Cancel", command=popup.destroy).pack()
+
+def save_popup():
+    content = text_box.get("1.0", tk.END)
+    popup = tk.Toplevel(root)
+    popup.title("Save")
+    popup.geometry("300x200")
+
+    label = tk.Label(popup, text="Enter File Name:")
+    label.pack()
+
+    entry = tk.Entry(popup, width=40)
+    entry.pack()
+
+    error_label = tk.Label(popup, text="", fg="red")
+    error_label.pack()
+
+    def on_save():
+        filename = entry.get().strip
+        if filename:
+            save_note(filename, content)
+            popup.destroy()
+        else:
+            error_label.config(text="Please enter a filename.")
+
+    tk.Button(popup, text="Save", command=on_save).pack()
+    tk.Button(popup, text="Cancel", command=popup.destroy).pack()
 
 if __name__ == '__main__':
 
@@ -30,8 +81,8 @@ if __name__ == '__main__':
     frame = tk.Frame(root)
     frame.pack()
 
-    tk.Button(frame, text='Save', command=save_note).pack(side=tk.LEFT, padx=5, pady=15)
-    tk.Button(frame, text='Load', command=load_note).pack(side=tk.LEFT, padx=5, pady=15)
+    tk.Button(frame, text='Save', command=save_popup).pack(side=tk.LEFT, padx=5, pady=15)
+    tk.Button(frame, text='Load', command=load_popup).pack(side=tk.LEFT, padx=5, pady=15)
     tk.Button(frame, text='Clear', command=clear_text_box).pack(side=tk.LEFT, padx=5, pady=15)
 
     root.mainloop()
